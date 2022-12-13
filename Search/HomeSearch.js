28d7dea8e17766e110924b27d7da67ac48e838a9
ridedie <ride_die@naver.com>
2022-12-13 23:53:20 +0900

    React Netflix

--- Search/HomeSearch.js
+++ Search/HomeSearch.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { API_URL, API_KEY, IMAGE_BASE_URL } from "../components/config";
 import Movie from "../components/Movie";
+import { useQuery } from "react-query";
 import { useSearchParams } from 'react-router-dom';
+import axios from 'axios';
+
+
+
 
 
 
 function HomeSearch() {
     const [searchs, setSearchs] = useState([]);
-    // const [movie_title, setMovie_title] = useState(" ");
     const [query, setQuery] = useSearchParams();
 
 
@@ -15,57 +19,75 @@ function HomeSearch() {
         setQuery(e.target.value)
     }
 
-    const getAPIUrl = useCallback(() => {
-        return  `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${query}`;
-    }, [query]);
 
-    useEffect(() => {
-        const endpoint = getAPIUrl();
-        fetchMovies(endpoint)
-        console.log(endpoint);
-    }, [getAPIUrl])
+    const fetchSearchAPI = (Search) => {
+        return axios.get(`${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${query}`);
+      };
+
+    const { data } = useQuery(['query', query ], fetchSearchAPI);
 
 
 
-    const fetchMovies = (endpoint) => {
-        fetch(endpoint)
-            .then((response) => response.json())
-            .then(response => {
-                setSearchs([...searchs, ...response.results]);
-            });
-    }
+    // useEffect(() => {
+    //     // useEffect는 비동기적으로 실행된다.
+    //     // query에 대한 검색 결과가 완료 되었는지를 검사할 변수 필요
+    //     let completed = false;
 
-    console.log(searchs);
+    //     async function get() {
+    //         const result = await axios(`${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${query}`)
+    //         if(!completed) {
+    //             setSearchs([...searchs, ...result.results]);
+    //         }
+    //     }
+
+    //     get()
+    //     return () => {
+    //         // 다른 비동기 작업이 또 실행되지 않도록
+    //         completed = true
+    //     }
+
+
+       // 두 번째 파라미터 []는 리액트 랜더링 조건
+       // 배열이 비어있다 -> 검사할 요소가 없다는 것
+       // 즉 이펙트를 실행할지 안 할지를 검사하는 변수
+       // 현재 상황에선 query가 바뀌는 시점이 된다.
+    // }, [query])
+
+
+
+
+    // const getAPIUrl = useCallback(() => {
+    //     return  `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${query}`;
+    // }, [query]);
+
+    // useEffect(() => {
+    //     const endpoint = getAPIUrl();
+    //     fetchMovies(endpoint)
+    //     console.log(endpoint);
+    //     return () => {
+    //         completed = true;
+    //     }
+    // }, [getAPIUrl])
+
+
+
+    // const fetchMovies = (endpoint) => {
+        // if(!completed) {
+        //     fetch(endpoint)
+        //     .then((response) => response.json())
+        //     .then(response => {
+        //         setSearchs([...searchs, ...response.results]);
+        //     });
+        // }
+        
+    // }
+
+   
     console.log(query);
 
     
 
 
-//   const [searchs, setSearch] = useState([]);
-//   const [query, setQuery] = useSearchParams();
-
-//   const onChange = (e) => {
-//     setQuery(e.target.value)
-// }
-
-//   const getProducts = async() => {
-//     let searchQuery = query.get('q') || "";
-//     console.log("쿼리무슨쿼리?", searchQuery)
-//     let endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchQuery}`;  //동적으로 바뀌는 정보 ${ }
-//     fetchMovies(endpoint)
-//   }
-
-//   const fetchMovies = (endpoint) => {
-//     fetch(endpoint)
-//         .then((response) => response.json())
-//         .then(response => {
-//             setSearch([...searchs, ...response.results]);
-//         });
-// }
-  
-//   useEffect(()=>{
-//       getProducts()
-//     }, [query])
 
 
 
@@ -73,7 +95,7 @@ function HomeSearch() {
     return (
         <div className="containers">
             <div>
-                <input type="text" value={query} onChange={onChange} minLength="1" placeholder='Title input' style={{ color: "white" }} />
+                <input type="text" value={query} onChange={onChange} minLength="1" placeholder='Title input' />
                 <input type="submit" value="확인"/>
             </div>
             {searchs && searchs.map((search, index) => {
