9ea00ae227c493b7e7ba8304fe69b61cc4c14e08
ridedie <ride_die@naver.com>
2022-12-15 23:42:04 +0900

    React Netflix

--- Search/HomeSearch.js
+++ Search/HomeSearch.js
@@ -1,8 +1,9 @@
-import React, { useState} from "react";
+import React, { useEffect, useState} from "react";
 import { API_URL, API_KEY, IMAGE_BASE_URL } from "../components/config";
 import Movie from "../components/Movie";
 import { useQuery , QueryClient, QueryClientProvider } from "react-query";
 import "./Search.css";
+import axios from 'axios';
 
 const queryClient = new QueryClient();
 
@@ -17,45 +18,79 @@ function HomeSearch() {
         setQuery(e.target.value)
     }
 
-    const fetchSearchAPI = async() => {
-		const json = await (
-			await fetch(
-				`${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${query}`
-			)
-		).json();
-		setMovie(json)
-	};
+    // const fetchSearchAPI = async() => {
+	// 	const json = await (
+	// 		await fetch(
+	// 			`${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${query}`
+	// 		)
+	// 	).json();
+	// 	setMovie(json)
+	// };
+
+
+   
+
+//     const fetchSearchAPI = () => {
+//         const endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${query}`;
+//         fetchMovies(endpoint)
+//         console.log(endpoint);
+//     }
+
+
+// const fetchMovies = (endpoint) => {
+//     fetch(endpoint)
+//         .then((response) => response.json())
+//         .then(response => {
+//             setMovie([...movies, ...response.results]);
+//         });
+// }
+
+
+    // const { data } = useQuery(['index', query],  fetchSearchAPI, {
+    //     enabled: !!query,
+    //     staleTime: 6 * 10 * 1000,
+    //     cacheTime: 6 * 10 * 1000
+    // });
 
 
 
-    const { data:  results } = useQuery(['index', query],  fetchSearchAPI, {
+const fetchSearchAPI = () => {
+    return axios.get(`${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${query}`);
+  };
+
+
+    
+    const { data } = useQuery(['index', query],  fetchSearchAPI, {
+        select: (data) => {
+            const movieQuery = data.data.results.map((search) => search);
+            return movieQuery;
+        },
         enabled: !!query,
         staleTime: 6 * 10 * 1000,
         cacheTime: 6 * 10 * 1000
     });
 
-
-    console.log(movies);
-    console.log(query);
+    console.log(data);    
+ 
+    
 
     return (
         <div className="containers">
             <div>
                 <input type="text" value={query} onChange={onChange} minLength="1" placeholder='Title input' />
-                <input type="submit" value="확인"/>
             </div>
-            { results &&  results.map((search, index) => {
+            { data && data.map((movieQuery, index) => {
                 return (
                     <React.Fragment key={index}>        
-                    {search.poster_path && (
+                    {movieQuery.poster_path && (
                           <Movie
                              image={
-                                 search.poster_path
-                                     ? `${IMAGE_BASE_URL}w500/${search.poster_path}`
+                                 movieQuery.poster_path
+                                     ? `${IMAGE_BASE_URL}w500/${movieQuery.poster_path}`
                                      : null
                              }
-                             key={search.id}
-                             id={search.id}
+                             key={movieQuery.id}
+                             id={movieQuery.id}
                          />
                     )
                            
