ac17d04265d429c6c4df02a937d7300bcbebd7c4
ridedie <ride_die@naver.com>
2022-12-21 23:40:43 +0900

    React Netflix

--- Detail/Detail.js
+++ Detail/Detail.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { API_URL, API_KEY, IMAGE_BASE_URL } from "../components/config";
 import Detail from "../components/Detail";
+import Movie from "../components/Movie";
 
 function BannerDetail() {
 	const { movieId } = useParams();
 	const [movies, setMovie] = useState([]);
+	const [More, setMore] = useState([]);
 	const navigate = useNavigate();
 	let genres = [];
 
@@ -24,12 +26,25 @@ function BannerDetail() {
 	}, [])
 
 
+
+	// useEffect(() => {
+    //     const endpoint = `${API_URL}/movie/popular?api_key=${API_KEY}&language=en-US`;
+    //     fetchMovies(endpoint)
+    // }, [])
+
+    // const fetchMovies = (endpoint) => {
+    //     fetch(endpoint)
+    //         .then((response) => response.json())
+    //         .then(response => {
+    //             setMore([...More, ...response.results]);
+    //         });
+    // }
+
+
 	// const navigateDramaMore = () => {
 	// 	navigate("/DramaMore");
 	// }
 
-
-	console.log(movies);
 	return (
 		<div>
 			<div className="Detail">
@@ -47,20 +62,37 @@ function BannerDetail() {
 								: null
 						}
 						overview={movies.overview}
-						let genresList = {genres.map((genres, i) => {
-							
-							 {genres.name}
-						})}
-						date={movies.release_date}
+						// let genresList = {genres.map((genres, i) => {
+						// 	return (
+						// 		{genres.name} 
+						// 	)
+						// })}
+						release_date={movies.release_date}
 						runtime={movies.runtime}
-						star={movies.vote_average}
+						vote_average={movies.vote_average}
 						key={movies.id}
 						id={movies.id}
 					/>
 					
 				</React.Fragment>
 			</div>
-
+			<div>
+			{/* {More.results.map((movie, index) => {
+							return (
+								<React.Fragment key={index}>
+									<Movie
+										image={
+											movie.poster_path
+												? `${IMAGE_BASE_URL}w500/${movie.poster_path}`
+												: null
+										}
+										key={movie.id}
+										id={movie.id}
+									/>
+								</React.Fragment>
+							);
+						})} */}
+			</div>
 			{/* <div className="DetailBtn">
 				<button className="Detailbutton" onClick={navigateDramaMore}>비슷한 영화</button>
 			</div> */}
--- components/Detail.js
+++ components/Detail.js
@@ -7,11 +7,15 @@ function Detail(props) {
             <img src={props.backdrop_path} alt={props.title} className='background' />
             <div className='rectangle'></div>
             <img src={props.poster_path} alt={props.title} className='image' />
-            <div className='MovieInfo'>
+            <div className='Info'>
                 <h3 className='title'>{props.title}</h3>
-                <p className='Info'>{props.release_date} ▪ {props.runtime} ▪ ⭐{props.vote_average}</p>
                 <p className='overview'>{props.overview}</p>
-                <h4>추천영화</h4>
+                <p className='genres'>{props.genres}</p>
+                <div className='MovieInfo'>
+                    <div><h4>Date</h4><p className='date'>{props.release_date}</p></div>
+                    <div><h4>runtime</h4> <p className='runtime'>{props.runtime}</p></div>
+                    <div><h4>Scope</h4> <p className='star'>{props.vote_average}/10</p></div>
+                </div>
             </div>
         </div>
 
