13d6530437cc3c1056a0e172c39258e6db088eb9
ridi <valuehjlee@gmail.com>
2024-05-26 23:56:45 +0900

    React Netflix

--- components/Banner.js
+++ components/Banner.js
@@ -1,40 +1,17 @@
 import { Link } from "react-router-dom"
+import './Banner.css';
 
 function Banner(props) {
 	return (
-		<div>
+		<div className="BgImg">
 			<Link to={`/Netflix/${props.id}`}>
-				<div
-					style={{
-						background: `linear-gradient(to bottom,rgba(0,0,0,0)
-			39%,rgba(0,0,0,0)
-			41%,rgba(0,0,0,0.65)
-			100%),
-			url(${props.image}), #1c1c1c`,
-						height: '100vh',
-						backgroundSize: '100%,cover',
-						width: '100%',
-						position: 'relative'
-					}}
-				>
+				
+			<div class="Bannercontainer">
+        		<img src={props.image} alt={props.title} class="Bannerimage" />
+				<h3 className="Bannertitle">{props.title}</h3>
+				<p className="BannerExplanation">{props.overview}</p>
+    		</div>
 
-						<div
-							style={{
-								position: "absolute",
-								maxWidth: "1000px",
-								margin: "200px",
-								bottom: "20px",
-								marginLeft: '150px',
-								fontSize: '32px'
-							}}
-						>
-
-							<h2 style={{ color: "white" }}> {props.title}</h2>
-
-
-						</div>
-
-				</div>
 			</Link>
 		</div>
 	);
